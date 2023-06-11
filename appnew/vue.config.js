ae4dece71217b7324c29b5a3a44695ba2cff1100
maksimpuhov <maks.pukhov@list.ru>
2023-06-11 19:14:25 +0500

    vue config js changed

--- appnew/vue.config.js
+++ appnew/vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-    proxy: 'http://192.168.3.2:3000/'
-  }
+    //proxy: 'http://192.168.3.2:3000/'
+    proxy: 'http://https://link-shortener-production-0bf0.up.railway.app:3000/'
+  },
+  //added to deploy
+  publicPath: 'https://https://link-shortener-production-0bf0.up.railway.app/',
 })
